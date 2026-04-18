d4d50935c3c574fb06e31f29c94d652ec80bdce5
muflih <muflihanu60@gmail.com>
2026-04-18 16:58:29 +0530

    portal

--- hotel_management/controller/food_order_controller.py
+++ hotel_management/controller/food_order_controller.py
@@ -1,5 +1,6 @@
 from odoo import http
 from odoo.http import request
+from odoo import Command
 
 class FoodOrderController(http.Controller):
     @http.route('/food_orders',type='http',auth="public",website=True)
@@ -9,13 +10,46 @@ class FoodOrderController(http.Controller):
 
     @http.route('/food_items', type='jsonrpc', auth="public", website=True)
     def food_order(self, category_values):
+        print(category_values)
         category_ids =[]
         for cat in category_values:
+            category_ids=[]
+            print(cat)
+            print('categorylist',category_ids)
             category_ids.append(int(cat))
         food_vals = []
-        foods = self.env['food.items'].search([('category_id', 'in', category_ids)])
+        foods = self.env['food.items'].sudo().search([('category_id', 'in', category_ids)])
         print(category_ids)
         print(foods)
         for food in foods:
-            food_vals.append({'food_id':food.id,'food_name': food.food_name, 'img':food.image,'food_qty':food.food_quantity})
+            food_vals.append({'food_id':food.id,'food_name': food.food_name, 'img':food.image,'food_qty':food.food_quantity,'food_price':food.food_price})
         return  {'food_vals': food_vals}
+
+    @http.route('/create_order', type='jsonrpc', auth="public", website=True)
+    def create_food_order(self,accommodation_id,food_names,food_qty,food_price):
+        vals=[]
+        line_vals=[]
+        print(food_names)
+        print(food_qty)
+        print(food_price)
+
+        # res=dict(zip(food_names,food_qty))
+        for f,q in zip(food_names,food_qty):
+            vals.append({'food_name':f,'food_qty':q})
+
+        for v,p in zip(vals,food_price):
+            line_vals.append((0,0,{
+                'order_name':v['food_name'],
+                'quantity':v['food_qty'],
+                'unit_price':p,
+                'subtotal':int(p)*v['food_qty'],
+            }))
+        print(accommodation_id)
+        print(vals)
+
+        print(123123,  )
+
+        created=self.env['order.food'].sudo().create({ 'accommodation_id':accommodation_id,
+                                                       'order_list_ids':line_vals})
+        print(created)
+
--- hotel_management/static/src/js/food_orders.js
+++ hotel_management/static/src/js/food_orders.js
@@ -9,29 +9,45 @@ export class FoodOrder extends Interaction {
 
         dynamicContent = {
 
-        ".category_ids": {
-            "t-on-change": (ev) =>this.food_items(ev),
-          },
+        // ".category_ids": {
+        //     "t-on-change": (ev) =>this.food_items(ev),
+        //   },
 
         ".order_btn": {
             "t-on-click": (ev) =>this._order_creating(ev),
-          }
+          },
+        "#btn_submit":{
+            't-on-click':(ev)=>this.submit_orders(ev),
+        }
+
       }
 
-
+    setup(){
+            $(".category_ids").chosen().change((ev) => this.food_items(ev));
+       this.food_names=[];
+       this.food_qty=[];
+       this.food_price=[];
+       this.category_error='';
+       this.qty_error_p='';
+    }
      async food_items(ev){
             this.form_data=new FormData(this.el)
           const category_values=this.form_data.getAll('category_ids')
+
+          console.log(123123, category_values)
            this.result = await rpc('/food_items', {category_values:category_values});
           const element=document.getElementById('food_orders_div');
+          element.innerHTML='';
          this.result.food_vals.forEach(function(text) {
 
-             element.insertAdjacentHTML("beforeend",`<div class="card  m-5" style="width: 10rem; align-items:center; height:250px;">
+
+             element.insertAdjacentHTML("beforeend",`<div class="card  m-5" style="width: 10rem; align-items:center; height:280px;">
                   
                                     <img class="card-img-top text-center" src="data:image/png;base64,${text.img}" alt="'food Image'" style="padding: 0px; margin: 0px; height: 100px; width:180px;"/>
                                    <h4 class="food_name mt-3">${text.food_name}</h4>
+                                    <h5 class="food_price mt-1 mb-2 text-center">${text.food_price}</h5>
                                     <div>
-                                  <input  name="qty" class="mb-4 qty" type="number" placeholder="qty" style="width:100px;"/>  
+                                  <input  name="qty" class="mb-2 qty" type="number" placeholder="Quantity" style="width:100px;"/>  
                                   </div>
                                   <button class="btn btn-primary order_btn mb-5">Done</buttom>
                                   <br/>
@@ -42,16 +58,55 @@ export class FoodOrder extends Interaction {
         }
     _order_creating(ev){
              ev.preventDefault();
-        const food_name=ev.target.previousElementSibling.previousElementSibling.innerHTML
+             this.qty_error_p=document.querySelector('#qty_error');
+              const order_div=document.getElementById('order_table');
+        const food_name=ev.target.previousElementSibling.parentElement.children[1].innerHTML
+       const  food_p=ev.target.previousElementSibling.parentElement.children[2].innerHTML
+        console.log('food_name',food_name)
          const  value=ev.target.previousElementSibling.firstElementChild.valueAsNumber
-           const order_div=document.getElementById('order_table');
-           order_div.insertAdjacentHTML("beforeend",`
+        console.log(ev.target.previousElementSibling.firstElementChild.valueAsNumber)
+        console.log('value qty',value)
+        if(isNaN(value)==true){
+            this.qty_error_p.innerHTML='please enter quantity';
+        }else{
+             this.qty_error_p.innerHTML='';
+              this.food_qty.push(value)
+             this.food_price.push(food_p)
+             this.food_names.push(food_name)
+            order_div.insertAdjacentHTML("beforeend",`
                                       <tr>
-                                <td>${food_name}</td>
-                                <td>${value}</td>
+                                <td style="border:1px solid; padding:5px;">${food_name}</td>
+                                <td  style="border:1px solid; padding:5px;">${value}</td>
                                  </tr>`)
+        }
+
+
 
 
     }
+
+     async submit_orders(ev){
+            ev.preventDefault();
+             this.form_data=new FormData(this.el)
+            this.accommodation=this.form_data.get('accommodation_id')
+            this.category_error=document.querySelector('.category_error');
+            const category_values=this.form_data.getAll('category_ids')
+             console.log(category_values)
+            if(category_values.length==0){
+                this.category_error.innerHTML='please select  a category';
+            }else{
+                this.category_error.innerHTML='';
+            }
+
+         console.log(this.accommodation)
+         console.log(this.food_names)
+        console.log(this.food_qty)
+         console.log(this.food_price)
+         await  rpc ('/create_order',{accommodation_id:this.accommodation,food_names:this.food_names,food_qty:this.food_qty,food_price:this.food_price})
+
+
+    }
+
+
 }
 registry.category("public.interactions").add("hotel_management.foodorder",FoodOrder);
